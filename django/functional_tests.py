fd2a4923fda9fe586cb3b87aed7c2b6a1dd1acb1
Yuchan Lee <dldbcks95@gmail.com>
2019-11-20 18:20:28 +0900

    edit: write essential functional tests for index page and make helper class get better

--- django/functional_tests.py
+++ django/functional_tests.py
@@ -14,7 +14,7 @@ from selenium.common.exceptions import NoSuchElementException
 @pytest.fixture(scope='session')
 def browser():
     browser = webdriver.Chrome(os.environ.get('SELENIUM_WEBDRIVER_CHROME', 'C:/Tools/chromedriver.exe'))
-    browser.implicitly_wait(3)
+    browser.implicitly_wait(1)
     yield browser
     browser.quit()
 
@@ -36,8 +36,30 @@ class BrowserTestHelper:
         for name in names:
             setattr(self, name, request.getfixturevalue(name))
 
+    @staticmethod
+    def is_url_pattern_matches(url, pattern):
+        """
+        receive absolute or relative url, and 'path' part will be compared to regex pattern
+        """
+        if isinstance(pattern, str):
+            p = re.compile(pattern)
+        elif isinstance(pattern, re.Pattern):
+            p = pattern
+        else:
+            raise TypeError('Expected type (str, re.Pattern) but received: {}'.format(type(pattern)))
+
+        return p.match(urlparse(url).path) is not None
+
     def get(self, url):
         return self.browser.get(urljoin(self.server_url, url))
+
+    def find_element_by_id(self, eid, default=None):
+        try:
+            element = self.browser.find_element_by_id(eid)
+        except NoSuchElementException:
+            return default
+        else:
+            return element
 
 
 # ==================================================================================================================== #
@@ -46,32 +68,73 @@ class BrowserTestHelper:
 class IndexPageTest(BrowserTestHelper):
     """
     tests for /, application home page
+    self.get() is being used multiple times because some tests may contain state change(hyperlinks, especially href)
     """
     page_url = '/'
 
-    def test_browser_ui_includes_menu(self):
-        self.get(self.page_url)
-
-    def test_user_enjoy_image_carousel(self):
-        # user get to the main page and find 'Take a Look' in title
+    def test_page_well_served(self):
+        # user visit main page and find 'Take a Look' in title
         self.get(self.page_url)
         assert 'Take a Look' in self.browser.title
 
+    def test_bottom_navigation_bar_works_well(self):
+        # user look layouts, especially our simple & fancy bottom bar
+        self.get(self.page_url)
+        navbar = self.find_element_by_id('bottom-navbar')
+        assert navbar is not None, 'Navigation bar is not found; did you forget?'
+
+        # using 'navbar' instead of 'self.browser' to make sure that link is child of navbar
+        # is navbar has index(home) link?
+        link_to_home = navbar.find_element_by_id('navbar-link-home')
+        assert self.is_url_pattern_matches(link_to_home.get_attribute('href'), '^/$')
+
+        # is navbar has link to history?
+        link_to_history = navbar.find_element_by_id('navbar-link-history')
+        assert self.is_url_pattern_matches(link_to_history.get_attribute('href'), '^/history/$')
+
+        # is navbar has link to model list?
+        link_to_model = navbar.find_element_by_id('navbar-link-model')
+        assert self.is_url_pattern_matches(link_to_model.get_attribute('href'), '^/model/$')
+
+    def test_user_enjoy_image_carousel(self):
         # user find image carousel
-        try:
-            carousel = self.browser.find_element_by_id('recent-submits')
-        except NoSuchElementException:
-            assert False, 'Carousel is not found'
+        self.get(self.page_url)
+        carousel = self.find_element_by_id('recent-submits')
+        assert carousel is not None, 'Carousel is not visible'
 
         # when user clicks image, then will be moved to its page (new state)
-        pattern = re.compile('^/history/*[0-9]/$')  # item pattern is expected to be integer!
-        carousel.click()
-        assert pattern.match(urlparse(self.browser.current_url).path) is not None
+        # the selenium tester decided to pick one what it see
+        links = carousel.find_elements_by_tag_name('a')
+        pattern = re.compile(r'^/history/\d+/$')
+        for link in links:
+            assert self.is_url_pattern_matches(link.get_attribute('href'), pattern)
+
+    def test_simple_descriptions_of_models_provided(self):
+        # finally user arrived at our models's preview
+        # it is some kind of drawer component, or accordion, or something like that
+        self.get(self.page_url)
+        preview = self.browser.find_element_by_id('model-previews')
+        assert preview is not None, 'Preview for models is not found'
+
+        # web will introduce them to model's detail description page by clicking its link
+        # and our tester will just pick first one
+        try:
+            model = preview.find_element_by_id('model-1')
+        except NoSuchElementException:
+            assert False, 'No models shown; is database or API alright?'
+
+        model.click()  # open accordion or collapsed
+        try:
+            link = model.find_element_by_id('model-1-link')
+        except NoSuchElementException:
+            assert False, 'No link found for model content; may view template or test malformed'
+
+        assert self.is_url_pattern_matches(link.get_attribute('href'), re.compile(r'^/model/\w+/$'))
 
 
 class HistoryPageTest(BrowserTestHelper):
     """
-    tests for /history, user submitted image list page
+    tests for /history/, user submitted image list page
     """
     page_url = '/history/'
 
@@ -85,7 +148,7 @@ class HistoryPageTest(BrowserTestHelper):
         assert False, 'Test is not done'
 
 
-class PredictionPageTest(BrowserTestHelper):
+class HistoryDetailPageTest(BrowserTestHelper):
     """
     tests for /history/:id/, user submitted image detail with specific informations about it
     """
@@ -94,4 +157,28 @@ class PredictionPageTest(BrowserTestHelper):
         return f'/history/{item}/'
 
     def test_(self):
-        assert False, 'Test is never written'
+        pass
+
+
+class ModelPageTest(BrowserTestHelper):
+    """
+    tests for /model/, list of machine learning models supported by server will be shown
+    """
+    page_url = '/model/'
+
+    def test_page_well_served(self):
+        self.get(self.page_url)
+        assert 'Take a Look' in self.browser.title
+
+
+class ModelDetailPageTest(BrowserTestHelper):
+    """
+    tests for /model/:name/, where detailed description of ML model provided
+    it is likely to include some visualization components
+    """
+    @staticmethod
+    def page_url(name):
+        return f'/model/{name}/'
+
+    def test_(self):
+        pass
